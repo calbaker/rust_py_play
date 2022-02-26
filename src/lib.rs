0314c203249c3ff381abb53781bc5c03eb527d74
Chad Baker <calbaker@gmail.com>
2022-02-26 14:55:42 -0700

    got it working with ndarray inside struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 // Things to figure out (TODO):
-// pass stuff into Rust as i32, 64, and lists of these
+// - [x] pass stuff into Rust as i32, 64, and lists of these
 // convert to ndarray and run sim
 // use serde to serialize classes so that you can programmatically change all 
 // arrays back to pyO3-compliant lists
@@ -10,20 +10,22 @@ use pyo3::prelude::*;
 use serde::Serialize;
 use ndarray::prelude::*;
 
+
 #[pyclass] 
 #[derive(Serialize)]
 struct MyStruct {
     a:i64,
     #[pyo3(get, set)]
     b:i64,
-    c:Vec<i64>,
+    c:Array1<i64>,
 }
 
 #[pymethods]
 impl MyStruct{
     #[new]
     fn __new__(a:i64, b:i64, c:Vec<i64>) -> Self {
-        MyStruct { a, b, c }
+        let c = Array::from_vec(c);
+        MyStruct { a, b, c}
     }    
     #[getter]
     fn mult(&self) -> PyResult<Vec<i64>>{
@@ -35,6 +37,13 @@ impl MyStruct{
     }
 }
 
+
+// Call the class using:
+// import py_play
+// import numpy as np
+// my_struct = py_play.MyStruct(a=0, b=5, c=np.arange(5)) # totally ok to pass in np.ndarray
+// my_struct.b # get
+// my_struct.mult # getter
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
