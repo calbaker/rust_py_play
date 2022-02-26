e0ead887c66a020ddfabc0003fb6e271c57a53dd
Chad Baker <calbaker@gmail.com>
2022-02-26 13:36:43 -0700

    able to take ints and output vec as list

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,40 @@
 
 use pyo3::prelude::*;
+use ndarray::prelude::*;
 
-// #[pyclass]
+#[pyclass]
+struct MyStruct {
+    arr0:Vec<i64>,
+    arr1:Vec<i64>
+}
+
+#[pymethods]
+impl MyStruct{
+    fn mult(&self) -> PyResult<Vec<i64>>{
+        assert_eq!(self.arr0.len(), self.arr1.len());
+        let mut vec = Vec::new();
+        for i in 0..self.arr0.len(){
+            vec.push(self.arr0[i] * self.arr1[i]);
+        }
+        Ok(vec)
+    }
+}
+
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn array_square(a: i64, b: i64) -> PyResult<Vec<i64>> {
+    let mut vec = Vec::new();
+    for i in a..b {
+        vec.push(i.pow(3));
+    }
+    Ok(vec)
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn string_sum(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+fn py_play(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(array_square, m)?)?;
+    // m.add_class(wrap_pyfunction!(MyStruct, m)?)?;
     Ok(())
 }
