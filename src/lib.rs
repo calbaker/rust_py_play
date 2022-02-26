592a75567c56753c855a306e2f4583de16f62f91
Chad Baker <calbaker@gmail.com>
2022-02-26 15:21:08 -0700

    proved out everything needed for rusty fastsim

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,46 @@
 // Things to figure out (TODO):
-// - [x] pass stuff into Rust as i32, 64, and lists of these
-// convert to ndarray and run sim
-// use serde to serialize classes so that you can programmatically change all 
-// arrays back to pyO3-compliant lists
-// pip stuff out
+// - [x] pass stuff into Rust as i32 and lists of these
+// - [x] convert to ndarray and run sim
+// - [x] use serde to serialize to pass EVERYTHING back to python if desired
 
 
 use pyo3::prelude::*;
 use serde::Serialize;
 use ndarray::prelude::*;
+// use std::collections::HashMap;
+
+#[derive(Serialize)]
 
 
 #[pyclass] 
-#[derive(Serialize)]
 struct MyStruct {
-    a:i64,
     #[pyo3(get, set)]
-    b:i64,
-    c:Array1<i64>,
+    c:i64,
+    arr:Array1<i64>,
 }
 
 #[pymethods]
 impl MyStruct{
     #[new]
-    fn __new__(a:i64, b:i64, c:Vec<i64>) -> Self {
-        let c = Array::from_vec(c);
-        MyStruct { a, b, c}
+    fn __new__(c:i64, arr:Vec<i64>) -> Self {
+        let arr = Array::from_vec(arr); // this allows us to use the pass vec as an ndarray
+        MyStruct { c, arr }
     }    
-    #[getter]
+
     fn mult(&self) -> PyResult<Vec<i64>>{
-        let mut vec = Vec::new();
-        for i in self.a..self.b{
-            vec.push(i * self.c[i as usize]);
-        }
-        Ok(vec)
+        Ok((&self.arr * self.c).to_vec())
+    }
+
+    fn to_json_str(&self) -> PyResult<String>{
+        Ok(serde_json::to_string(self).unwrap())
     }
 }
 
 
-// Call the class using:
+// Call the struct using:
 // import py_play
 // import numpy as np
-// my_struct = py_play.MyStruct(a=0, b=5, c=np.arange(5)) # totally ok to pass in np.ndarray
+// my_struct = py_play.MyStruct(c=5, arr=np.arange(5)) # totally ok to pass in np.ndarray or list
 // my_struct.b # get
 // my_struct.mult # getter
 
